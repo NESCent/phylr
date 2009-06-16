b9a92e116e387096e91703368758499d04499771
Dazhi Jiao <dazhi.jiao@gmail.com>
2009-06-16 03:39:40 +0000

    Initial import

---
+++ phylr-gsoc/src/main/java/ORG/oclc/os/SRW/Lucene/DCRecordResolver.java
@@ -0,0 +1,33 @@
+package ORG.oclc.os.SRW.Lucene;
+
+import gov.loc.www.zing.srw.ExtraDataType;
+
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.document.Document;
+
+import ORG.oclc.os.SRW.Record;
+
+public class DCRecordResolver implements RecordResolver {
+    static Log log=LogFactory.getLog(DCRecordResolver.class);
+    static final String SCHEMA_ID="info:srw/schema/1/dc-v1.1";
+    
+	public void init(Properties properties) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Record resolve(Document doc, String IdFieldName,
+			ExtraDataType extraDataType) {
+        StringBuffer sb = new StringBuffer("<dc xmlns:dc=\"http://purl.org/dc/elements/1.1/\">");
+        
+        sb.append("<dc:creator>");
+        sb.append(doc.getField("authors").stringValue());
+        sb.append("</dc:creator>");
+        sb.append("</dc>");
+        return new Record(sb.toString(), SCHEMA_ID);
+	}
+
+}
