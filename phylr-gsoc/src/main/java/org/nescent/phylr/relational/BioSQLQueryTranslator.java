50a507f747d20b46b9abce322f651d64df5017d3
Dazhi Jiao <dazhi.jiao@gmail.com>
2009-07-29 20:28:23 +0000

    Move SQL statements from code to prop file

--- phylr-gsoc/src/main/java/org/nescent/phylr/relational/BioSQLQueryTranslator.java
+++ phylr-gsoc/src/main/java/org/nescent/phylr/relational/BioSQLQueryTranslator.java
@@ -1,6 +1,10 @@
 package org.nescent.phylr.relational;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
@@ -17,8 +21,9 @@ import org.z3950.zing.cql.CQLTermNode;
 public class BioSQLQueryTranslator implements CqlQueryTranslator {
     private static Log log= LogFactory.getLog(BioSQLQueryTranslator.class);
     SRWRelationalDatabase database = null;
+    Map<String, String> indexMapping = null;
     
-    private String outSql = "select tree_id, xmlagg(xvalue) from ( " +
+    private String outTreeSql = "select tree_id, xmlagg(xvalue) from ( " +
     		"SELECT tree_id, xmlforest(tq.value as \"dc:identifier\") as xvalue " +
     		"FROM tree_qualifier_value tq, term te " +
     		"WHERE tq.term_id = te.term_id and te.name='dc.identifier' " +
@@ -37,30 +42,31 @@ public class BioSQLQueryTranslator implements CqlQueryTranslator {
     		") as tab " +
     		"WHERE tree_id in (?) " + 
     		"GROUP BY tree_id";
+    private String outNodeSql = "";
     
-    private String ftSql = "select tq.tree_id from tree_qualifier_value as tq, term as te " +
-    		"where to_tsvector('english', value) @@ to_tsquery('english', ?) " +
-    		"and tq.term_id = te.term_id and te.name=?";
-    
-    private String eqSql = "select tq.tree_id from tree_qualifier_value as tq, term as te " +
-			"where value=? " +
-			"and tq.term_id = te.term_id and te.name=?";    
-
-// Not legal until JDK 6    @Override
     public void init(Properties properties, SRWRelationalDatabase ldb) throws InstantiationException {
     	this.database = ldb;
+    	this.indexMapping = new HashMap<String, String>();
+    	
+        Enumeration enumer=properties.propertyNames();
+        while(enumer.hasMoreElements()) {
+            String prop=(String)enumer.nextElement();
+            if(prop.startsWith("qualifier.")) {
+            	indexMapping.put(prop.substring(10), properties.getProperty(prop));
+            }
+        }
+        
+        this.outTreeSql = properties.getProperty("SRWRelationalDatabase.query.tree");
+        this.outNodeSql = properties.getProperty("SRWRelationalDatabase.query.node");
     }
 
-// Not legal until JDK 6    @Override
     public String makeQuery(CQLNode node, SortTool sortTool) throws SRWDiagnostic {
-    	String sql = outSql;
     	StringBuffer sb =new StringBuffer();
     	makeSQLQuery(node, sb);
-    	sql = sql.replaceFirst("\\?", sb.toString());
-    	return sql;
+    	return outTreeSql.replaceFirst("\\?", sb.toString());
     }
     
-    public void makeSQLQuery(CQLNode node, StringBuffer sb) {
+    public void makeSQLQuery(CQLNode node, StringBuffer sb) throws SRWDiagnostic {
         if(node instanceof CQLBooleanNode) {
             CQLBooleanNode cbn=(CQLBooleanNode)node;
             makeSQLQuery(cbn.left, sb);
@@ -72,32 +78,31 @@ public class BioSQLQueryTranslator implements CqlQueryTranslator {
                 sb.append(" UNION ");
             else sb.append(" UnknownBoolean("+cbn+") ");
             makeSQLQuery(cbn.right, sb);
-        }
-        else if(node instanceof CQLTermNode) {
+        } else if(node instanceof CQLTermNode) {
         	String sql = null;
             CQLTermNode ctn=(CQLTermNode)node;
             String index=ctn.getIndex();
-            if(index.equals(""))
-               index = "dc.title"; // set default field to dc:title
+            if(index.equals("") || index.equals("cql.serverChoice")) {
+            	index = "dc.title"; // set default field to dc:title
+            }
 
             String term = ctn.getTerm();
-
+            if (!indexMapping.containsKey(index)) {
+            	throw new SRWDiagnostic(SRWDiagnostic.UnsupportedIndex, index);
+            }
+        	sql = indexMapping.get(index);
             if(ctn.getRelation().getBase().equals("=") ||
               ctn.getRelation().getBase().equals("scr")) {
-            	sql = eqSql.replaceFirst("\\?", "'" + term + "'");
+            	sql = sql.replaceFirst("\\?", "'" + term + "'");
+            } else if(ctn.getRelation().getBase().equals("any")) {
+            	sql = sql.replaceFirst("\\?", "'" + StringUtils.join(StringUtils.split(term), " | ") + "'");
+            } else if(ctn.getRelation().getBase().equals("all")) {
+            	sql = sql.replaceFirst("\\?", "'" + StringUtils.join(StringUtils.split(term), " & ") + "'");
+            } else if (ctn.getRelation().getBase().equals("exact")) {
+            	sql = sql.replaceFirst("\\?", "'" + term + "'");
             } else {
-            	sql = ftSql;
-                if(ctn.getRelation().getBase().equals("any")) {
-                	sql = sql.replaceFirst("\\?", "'" + StringUtils.join(StringUtils.split(term), " | ") + "'");
-                }
-                else if(ctn.getRelation().getBase().equals("all")) {
-                	sql = sql.replaceFirst("\\?", "'" + StringUtils.join(StringUtils.split(term), " & ") + "'");
-                } else if (ctn.getRelation().getBase().equals("exact")) {
-                	sql = sql.replaceFirst("\\?", "'" + term + "'");
-                } else
-                    sql = "Unsupported Relation: "+ctn.getRelation().getBase();
+            	sql = "Unsupported Relation: "+ctn.getRelation().getBase();
             }
-            sql = sql.replaceFirst("\\?", "'" + index + "'");
             sb.append(sql);
         }
         else sb.append("UnknownCQLNode("+node+")");
